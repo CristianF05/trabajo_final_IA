e356cec754e8703ffb94549ca1e0a7a71cb392d2
CristianF05 <1482670@senati.pe>
2024-02-28 19:25:38 -0500

    desarrollo del login y pagina principal

---
+++ login/interacion.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const loginForm = document.getElementById('loginForm');
+    loginForm.addEventListener('submit', function(event) {
+        // Evitar el envío del formulario por defecto
+        event.preventDefault();
+        
+        // Obtener los valores de usuario y contraseña
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        // Aquí podrías agregar lógica de verificación del usuario y contraseña
+        // Por simplicidad, redireccionamos al usuario a la página principal
+        window.location.href = '../pagina_principal/pagina_principal.html';
+    });
+});
